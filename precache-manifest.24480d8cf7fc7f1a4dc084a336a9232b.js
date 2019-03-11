1637edd32c85283c0b215baa1580817872993058
ignacio-cortes-externo <ignacio.cortes.externo@axa.com>
2019-03-11 17:27:55 +0100

    Updates

---
+++ precache-manifest.24480d8cf7fc7f1a4dc084a336a9232b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tasks/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f8411b302e0a2045f8d",
+    "url": "/react-tasks/static/js/runtime~main.da0dcbc5.js"
+  },
+  {
+    "revision": "47a2713efdc6261960e0",
+    "url": "/react-tasks/static/js/main.cc29fc8f.chunk.js"
+  },
+  {
+    "revision": "22e317750fe535ebb896",
+    "url": "/react-tasks/static/js/2.b9710498.chunk.js"
+  },
+  {
+    "revision": "47a2713efdc6261960e0",
+    "url": "/react-tasks/static/css/main.cfbd3e48.chunk.css"
+  },
+  {
+    "revision": "c72c7ea619b63299c258a5a7231ca69d",
+    "url": "/react-tasks/index.html"
+  }
+];
